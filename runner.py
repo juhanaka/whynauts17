dc69f7943c58a26f1e927e9f3cd9707d784fb41b
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2017-07-16 22:49:58 -0700

    Stuff

--- runner.py
+++ runner.py
@@ -41,6 +41,7 @@ class Runner():
         s = collector.get_stats()
         if not self.should_publish(pin_index, s[self.kMeanIndex], s[self.kDotIndex], s[self.kDotDotIndex]):
           continue
-        dot_value = min(1.0, self.kDotScale * s[self.kDotIndex])
-        self.publisher.publish_note_on(pin_index, dot_value)
+        if dot_value > 0.0:
+          dot_value = min(1.0, self.kDotScale * s[self.kDotIndex])
+          self.publisher.publish_note_on(pin_index, dot_value)
         self.publisher.publish_control_change(pin_index, s[self.kMeanIndex])
--- running_stats.py
+++ running_stats.py
@@ -12,9 +12,9 @@ class RunningStatsCollector():
 
   def get_stats(self):
     mean = sum(self.values) / float(len(self.values))
-    dot = map(abs, np.gradient(self.values) if len(self.values) > 1 else [0])
+    dot = np.gradient(self.values) if len(self.values) > 1 else [0]
     mean_dot = float(sum(dot)) / len(dot)
-    dot_dot = map(abs, np.gradient(dot) if len(dot) > 1 else [0])
+    dot_dot = np.gradient(dot) if len(dot) > 1 else [0]
     mean_dot_dot = float(sum(dot_dot)) / len(dot_dot)
-    return (mean, abs(mean_dot), abs(mean_dot_dot))
+    return (mean, mean_dot, mean_dot_dot)
 
