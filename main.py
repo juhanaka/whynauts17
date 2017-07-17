265f22fb39838a5f1755bbe012c72b76b1f922b3
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2017-07-16 17:25:26 -0700

    Stuff

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import runner
 
 def main():
-  window_length = 10
+  window_length = 5
   min_diff = 0.001
-  tick_seconds = 0.1
+  tick_seconds = 0.01
   r = runner.Runner()
   r.run(window_length, min_diff, tick_seconds)
 
--- runner.py
+++ runner.py
@@ -5,12 +5,21 @@ import threading
 import time
 
 class Runner():
+  kDotThreshold = 0.001
+  kMeanIndex = 0
+  kDotIndex = 1
+  kDotDotIndex = 2
+
   def __init__(self):
     self.reader = fsr_reader.FsrReader()
     self.collectors = {}
     self.collectors_mutex = threading.Lock()
-    self.previous_stats = {}
     self.publisher = midi_publisher.MidiPublisher()
+
+  def should_publish(self, pin_index, mean, dot, dot_dot):
+    if dot > self.kDotThreshold:
+      return True
+    return False
 
   def reader_loop(self, window_length):
     while True:
@@ -24,6 +33,7 @@ class Runner():
 
   def run(self, window_length, min_diff, tick_seconds):
     reader = threading.Thread(target=self.reader_loop, args=(window_length,))
+    reader.daemon = True
     reader.start()
 
     while True:
@@ -34,13 +44,8 @@ class Runner():
           stats[pin_index] = collector.get_stats()
 
       for pin_index, stats in stats.iteritems():
-        for i, stat in enumerate(stats):
-          should_publish = True
-          if (pin_index, i) in self.previous_stats:
-            previous_stat = self.previous_stats[(pin_index, i)]
-            if abs(previous_stat - stat) < min_diff:
-              should_publish = False
-          if should_publish:
-            self.publisher.publish(pin_index, i, stat)
-          self.previous_stats[(pin_index, i)] = stat
-    reader.join()
+        if not self.should_publish(pin_index, stats[self.kMeanIndex], stats[self.kDotIndex], stats[self.kDotDotIndex]):
+          continue
+        self.publisher.publish(pin_index, self.kMeanIndex, stats[self.kMeanIndex])
+        self.publisher.publish(pin_index, self.kDotIndex, stats[self.kDotIndex])
+        self.publisher.publish(pin_index, self.kDotDotIndex, stats[self.kDotDotIndex])
