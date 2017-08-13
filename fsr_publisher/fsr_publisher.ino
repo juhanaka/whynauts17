790b6d3c2797b0704cc2dbe3c15f452f883bbd11
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2017-08-13 16:10:47 -0700

    Arduino script baudrate etc.

--- fsr_publisher/fsr_publisher.ino
+++ fsr_publisher/fsr_publisher.ino
@@ -1,23 +1,22 @@
-#define NUM_PINS    4
-
-const int kBaudRate = 9600;
+const long kBaudRate = 115200;
 const char kDelimiter = ',';
-const int kStartPin = 8;
-const int kDelayMs = 50;
+const int kStartPin = 0;
+const int kDelayMs = 12;
+const int kNumPins = 16;
 
-int values[NUM_PINS];
+int values[kNumPins];
 
 void setup()
-{    
-  for (int i = 0; i < NUM_PINS; ++i) {
+{
+  for (int i = 0; i < kNumPins; ++i) {
     values[i] = 0;
   }
-  
+
   Serial.begin(kBaudRate);
 }
 
 void loop() {
-  for (int i = 0; i < NUM_PINS; ++i) {
+  for (int i = 0; i < kNumPins; ++i) {
     int fsr_reading = analogRead(kStartPin + i);
     int oldValue = values[i];
     if (oldValue != fsr_reading) {
