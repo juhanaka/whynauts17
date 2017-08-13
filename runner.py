bbeaa18398d36342a33bf750e7f09dca3a7af52b
brianbolze <brianbolze@gmail.com>
2017-08-12 21:52:05 -0700

    Fixed bug in note on lists. Improved note triggering behavior

--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@ import fsr_reader
 import running_stats
 import midi_publisher
 import threading
-import time
+from time import time
 
 class Runner():
   kDotThreshold = 0.01
@@ -17,14 +17,14 @@ class Runner():
   kNumNotes = 16
   kNumChannels = 3
   kNoteOnThreshold = 0.3
-  kNoteOffThreshold = 0.3
 
 
   def __init__(self):
     self.reader = fsr_reader.FsrReader()
     self.collectors = {}
     self.publisher = midi_publisher.MidiPublisher()
-    self.notesOn = [[False] * self.kNumNotes] * self.kNumChannels
+    self.notesOn = [[False] * self.kNumNotes for _ in range(self.kNumChannels)]
+    self.noteTimes = [[time()-60] * self.kNumNotes for _ in range(self.kNumChannels)]
     self.last_decay = [0] * self.kNumNotes
 
   def should_publish_control(self, pin_index, mean, dot, dot_dot):
@@ -58,47 +58,75 @@ class Runner():
   def check_for_basic_note_off(self, pin_index, mean, dot, ch):
     if not self.notesOn[ch-1][pin_index]:
       return False
-    if mean <= self.kNoteOffThreshold or self.last_decay[pin_index] < 0.05:
+
+    time_since = time()-self.noteTimes[ch-1][pin_index]
+    if mean <= 0.3 or time_since > 6:
       self.notesOn[ch-1][pin_index] = False
       self.publisher.publish_note_off(pin_index, 0.5, ch)
+      print 'Basic Note Off: ', pin_index, ' Channel: ', ch
       return True
     return False
 
-  def check_for_activity_based_note_off(self, pin_index, change_decay, ch):
+  def check_for_activity_based_note_off(self, pin_index, mean, dot, ch):
     if not self.notesOn[ch-1][pin_index]:
       return False
-    if change_decay < 0.05:
+
+    if mean <= 0.3 or self.last_decay[pin_index] < 0.05:
       self.notesOn[ch-1][pin_index] = False
       self.publisher.publish_note_off(pin_index, 0.5, ch)
+      print 'Activity Note Off: ', pin_index, ' Channel: ', ch
       return True
     return False
 
-  def check_for_pressure_note_on(self, pin_index, mean, dot, ch=1):
+  def check_for_time_based_note_off(self, pin_index, delay, ch):
+    if not self.notesOn[ch-1][pin_index]:
+      return False
+
+    time_since = time()-self.noteTimes[ch-1][pin_index]
+    if time_since > delay:
+      self.notesOn[ch-1][pin_index] = False
+      self.publisher.publish_note_off(pin_index, 0.5, ch)
+      print 'Time Based Note Off: ', pin_index, ' Channel: ', ch
+      return True
+    return False
+
+  def check_for_pressure_note_on(self, pin_index, mean, dot, ch):
     if self.notesOn[ch-1][pin_index]:
       return False
-    if mean >= self.kNoteOnThreshold and dot >= 0.016:
+
+    time_since_activity_trigger = time()-self.noteTimes[2][pin_index]
+    if mean >= self.kNoteOnThreshold and dot >= 0.016 and time_since_activity_trigger > 12:
       self.notesOn[ch-1][pin_index] = True
+      self.noteTimes[ch-1][pin_index] = time()
       velocity = min(1.0, self.kDotScale * dot)
       self.publisher.publish_note_on(pin_index, velocity, ch)
+      print 'Pressure Note On: ', pin_index, ' Channel: ', ch
       return True
     return False
 
-  def check_for_velocity_note_on(self, pin_index, mean, dot, ch=2):
+  def check_for_velocity_note_on(self, pin_index, mean, dot, ch):
     if self.notesOn[ch-1][pin_index]:
       return False
+
     if mean >= 0.15 and dot >= 0.06:
       self.notesOn[ch-1][pin_index] = True
+      self.noteTimes[ch-1][pin_index] = time()
       velocity = min(1.0, self.kDotScale * dot)
       self.publisher.publish_note_on(pin_index, velocity, ch)
+      print 'Velocity Note On: ', pin_index, ' Channel: ', ch
       return True
     return False
 
-  def check_for_activity_based_note_on(self, pin_index, change_decay, ch=3):
+  def check_for_activity_based_note_on(self, pin_index, change_decay, ch):
     if self.notesOn[ch-1][pin_index]:
       return False
-    if change_decay > 0.6:
+
+    time_since = time()-self.noteTimes[ch-1][pin_index]
+    if change_decay > 0.45 and time_since > 6:
       self.notesOn[ch-1][pin_index] = True
+      self.noteTimes[ch-1][pin_index] = time()
       self.publisher.publish_note_on(pin_index, 0.7, ch)
+      print 'Activity Note On: ', pin_index, ' Channel: ', ch
       return True
     return False
 
@@ -125,12 +153,17 @@ class Runner():
           self.check_for_basic_note_off(pin_index, s[self.kMeanIndex], s[self.kDotIndex], ch = 1)
 
         if not self.check_for_velocity_note_on(pin_index, s[self.kMeanIndex], s[self.kDotIndex], ch = 2):
-          # self.check_for_activity_based_note_off(pin_index, decay_value, ch = 2)
-          self.check_for_basic_note_off(pin_index, s[self.kMeanIndex], s[self.kDotIndex], ch = 2)
+          self.check_for_activity_based_note_off(pin_index, s[self.kMeanIndex], s[self.kDotIndex], ch = 2)
         
         if not self.check_for_activity_based_note_on(pin_index, decay_value, ch = 3):
-          # self.check_for_activity_based_note_off(pin_index, decay_value, ch = 3)
-          self.check_for_basic_note_off(pin_index, s[self.kMeanIndex], s[self.kDotIndex], ch = 3)
+          self.check_for_time_based_note_off(pin_index, delay = 4, ch = 3)
+        else:
+          if self.notesOn[0][pin_index]:
+            self.notesOn[0][pin_index] = False
+            self.publisher.publish_note_off(pin_index, 0.8, 1)
+          if self.notesOn[1][pin_index]:
+            self.notesOn[1][pin_index] = False
+            self.publisher.publish_note_off(pin_index, 0.8, 2)
 
 
         if self.should_publish_change_decay(pin_index, decay_value):
